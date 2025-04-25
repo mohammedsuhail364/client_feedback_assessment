07efdbf7256a3f99a75b5fa1ce3f8880b8f87065
mohammedsuhail364 <mohammedthanish364@gmail.com>
2025-04-25 15:23:24 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import SubmitFeedback from "./pages/SubmitFeedback";
 import AdminDashboard from "./pages/AdminDashboard";
-import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/feedback" element={<SubmitFeedback />} />
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
